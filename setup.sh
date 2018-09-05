521afebc99cfe58206c8bf7022a0b104883ec8a7
Valery Barysok <valery.barysok@viv.health>
2018-09-05 17:21:18 +0300

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:valery-barysok/goland-vcs-detection-issue-module-1.git main/src/github.com/valery-barysok/goland-vcs-detection-issue-module-1
+git clone git@github.com:valery-barysok/goland-vcs-detection-issue-module-2.git main/src/github.com/valery-barysok/goland-vcs-detection-issue-module-2
